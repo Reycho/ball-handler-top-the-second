fbabc48281d350b2b33933ca027e6c3f5118ce76
Reycho <62747204+Reycho@users.noreply.github.com>
2022-06-28 15:07:50 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,59 @@
+input.onGesture(Gesture.TiltLeft, function () {
+    radio.sendValue("tleft", 0)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        `)
+    basic.pause(350)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.TiltRight, function () {
+    radio.sendValue("tright", 0)
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        `)
+    basic.pause(350)
+    basic.clearScreen()
+})
+basic.showLeds(`
+    # # # . .
+    . . # # #
+    . . # . .
+    . # . . .
+    . # . . .
+    `)
+let vol = 0
+music.setBuiltInSpeakerEnabled(true)
+radio.setGroup(69)
+basic.forever(function () {
+    music.setVolume(vol)
+    led.plotBarGraph(
+    vol,
+    255
+    )
+    if (input.buttonIsPressed(Button.A)) {
+        vol += -5
+        if (vol < 0) {
+            vol = 0
+        }
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        vol += 5
+        if (vol > 255) {
+            vol = 255
+        }
+    }
+})
+basic.forever(function () {
+    led.toggle(2, 2)
+    basic.pause(100)
+})
